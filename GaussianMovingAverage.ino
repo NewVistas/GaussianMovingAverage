a5cc317b214bddad130a9794e78ff9cb473d8935
Nate Hopkins <smartsystemdesign@gmail.com>
2016-07-28 12:15:50 -0600

    Removed the change threshold for numSamples.  Also, removed globals numSamples and gaussianCurveCutoff.

--- GaussianMovingAverage.ino
+++ GaussianMovingAverage.ino
@@ -1,14 +1,11 @@
 const uint8_t analogDataPin = 0; // connect to a pot (simulates the analog signal)
 const uint8_t numSamplesPin = 2; // connect to a pot (controls the number of sample points that are averaged.  The width of the gaussian curve spans the number of points averaged)
 const uint8_t cutoffPin = 3; // connect to a pot (controls how near the edges of a fixed gaussian curve we use for weights.  Doesn't control the variance or standard deviation)
-const uint8_t changeNumSamplesThreshold = 1; // set to a value between minNumSamples (most resolution) and the value of maxNumSamples (least resolution)
 const uint8_t minNumSamples = 5;
 const uint8_t maxNumSamples = 255; // you can increase this (max of 255) if you want to be able to average more data points
 const float minCutoff = 3.0;
 const float maxCutoff = 8.0;
 
-float gaussianCurveCutoff; // how wide the gaussian curve is (number of standard deviations to either side of the center)
-uint8_t numSamples;
 uint16_t analogData[maxNumSamples];
 uint16_t currentSampleData;
 uint16_t middleSample;
@@ -26,22 +23,16 @@ void setup() {
 	pinMode(analogDataPin, INPUT);
 	pinMode(numSamplesPin, INPUT);
 	pinMode(cutoffPin, INPUT);
-	numSamples = map(analogRead(numSamplesPin), 0, 1023, minNumSamples, maxNumSamples);
 }
 
 void loop() {
-
 	// TODO: optimize this like numSamples, but put mapf inside if statement, as well
-	gaussianCurveCutoff = mapf(analogRead(cutoffPin), 0, 1023, minCutoff, maxCutoff);
+	float gaussianCurveCutoff = mapf(analogRead(cutoffPin), 0, 1023, minCutoff, maxCutoff); // how wide the gaussian curve is (number of standard deviations to either side of the center)
+	uint8_t numSamples = map(analogRead(numSamplesPin), 0, 1023, minNumSamples, maxNumSamples);
 
 	if(mode == printWeights) {
 		printGaussianWeights(numSamples, gaussianCurveCutoff);
 		while(1);
-	}
-
-	uint8_t numSamplesReading = map(analogRead(numSamplesPin), 0, 1023, minNumSamples, maxNumSamples);
-	if(abs(numSamplesReading - numSamples) >= changeNumSamplesThreshold) { // only change numSamples when the pot reading has changed more than changeNumSamplesThreshold
-		numSamples = numSamplesReading;
 	}
 
 	if(mode == plotData) {
@@ -69,12 +60,12 @@ void loop() {
 	}
 }
 
-float getGaussianAverage(uint16_t analogData[], uint8_t numberOfSamples, float gaussianCurveCutoff) {
+float getGaussianAverage(uint16_t analogData[], uint8_t numSamples, float gaussianCurveCutoff) {
 	float gaussianSum = 0;
 	float sum = 0;
 
-	for(uint8_t i = 0; i < numberOfSamples; i++) {
-		float gaussianWeight = calculateGaussian(i, numberOfSamples, gaussianCurveCutoff);
+	for(uint8_t i = 0; i < numSamples; i++) {
+		float gaussianWeight = calculateGaussian(i, numSamples, gaussianCurveCutoff);
 		gaussianSum += (analogData[i]*gaussianWeight);
 		sum += gaussianWeight;
 	}
@@ -85,23 +76,23 @@ float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-float calculateGaussian(uint8_t currentPoint, uint8_t numberOfSamples, float gaussianCurveCutoff) {
-	float a = (currentPoint*gaussianCurveCutoff/(numberOfSamples-1) - gaussianCurveCutoff/2);
+float calculateGaussian(uint8_t currentPoint, uint8_t numSamples, float gaussianCurveCutoff) {
+	float a = (currentPoint*gaussianCurveCutoff/(numSamples-1) - gaussianCurveCutoff/2);
 	return exp(-0.5f * a * a);
 }
 
-void printGaussianWeights(uint8_t numberOfSamples, float gaussianCurveCutoff) {
+void printGaussianWeights(uint8_t numSamples, float gaussianCurveCutoff) {
 	float gaussianWeights[numSamples];
 	float sum = 0;
 
-	for(uint8_t i = 0; i < numberOfSamples; i++) {
+	for(uint8_t i = 0; i < numSamples; i++) {
 		gaussianWeights[i] = calculateGaussian(i, numSamples, gaussianCurveCutoff);
 		sum += gaussianWeights[i];
 	}
 
 	Serial.println();
 	Serial.println();
-	for(uint8_t i = 0; i < numberOfSamples; i++) {
+	for(uint8_t i = 0; i < numSamples; i++) {
 		Serial.println(gaussianWeights[i] / sum, 7);
 	}
 
