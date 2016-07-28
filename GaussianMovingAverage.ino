a40432f5957908d40c4665120fabea3d15711932
Nate Hopkins <smartsystemdesign@gmail.com>
2016-07-28 16:07:47 -0600

    Optimized plotData mode to cache gaussian weights and sum when pots aren't being moved.  They will be recalculated again when the a pot is moved beyond the cutoffThreshold or numSamplesThreshold.

--- GaussianMovingAverage.ino
+++ GaussianMovingAverage.ino
@@ -2,13 +2,20 @@ const uint8_t analogDataPin = 0; // connect to a pot (simulates the analog signa
 const uint8_t numSamplesPin = 2; // connect to a pot (controls the number of sample points that are averaged.  The width of the gaussian curve spans the number of points averaged)
 const uint8_t cutoffPin = 3; // connect to a pot (controls how near the edges of a fixed gaussian curve we use for weights.  Doesn't control the variance or standard deviation)
 const uint8_t minNumSamples = 5;
-const uint8_t maxNumSamples = 255; // you can increase this (max of 255) if you want to be able to average more data points
+const uint8_t maxNumSamples = 150; // you can increase this (max of 255, but you may run low on memory) if you want to be able to average more data points
 const float minCutoff = 3.0;
 const float maxCutoff = 8.0;
+const float cutoffThreshold = 0.02; // you must change the cutoff pot more than this amount to recalculate gaussian curve
+const uint8_t numSamplesThreshold = 1; // you must change the numSamples pot more than this amount to recalculate gaussian curve
 
 uint16_t analogData[maxNumSamples];
 uint16_t currentSampleData;
 uint16_t middleSample;
+bool recalculate = true;
+float gaussianCurveCutoff;
+float lastGaussianCurveCutoff;
+uint8_t numSamples;
+uint8_t lastNumSamples;
 
 enum Mode {
 	plotCurve,
@@ -26,9 +33,18 @@ void setup() {
 }
 
 void loop() {
-	// TODO: optimize this like numSamples, but put mapf inside if statement, as well
-	float gaussianCurveCutoff = mapf(analogRead(cutoffPin), 0, 1023, minCutoff, maxCutoff); // how wide the gaussian curve is (number of standard deviations to either side of the center)
-	uint8_t numSamples = map(analogRead(numSamplesPin), 0, 1023, minNumSamples, maxNumSamples);
+	gaussianCurveCutoff = mapf(analogRead(cutoffPin), 0, 1023, minCutoff, maxCutoff); // how wide the gaussian curve is (number of standard deviations to either side of the center)
+	numSamples = map(analogRead(numSamplesPin), 0, 1023, minNumSamples, maxNumSamples);
+
+	if(abs(gaussianCurveCutoff - lastGaussianCurveCutoff) < cutoffThreshold && 
+		abs(numSamples - lastNumSamples) < numSamplesThreshold) {
+		recalculate = false;
+	}
+	else {
+		lastGaussianCurveCutoff = gaussianCurveCutoff;
+		lastNumSamples = numSamples;
+		recalculate = true;
+	}
 
 	if(mode == printWeights) {
 		printGaussianWeights(numSamples, gaussianCurveCutoff);
@@ -37,20 +53,20 @@ void loop() {
 
 	if(mode == plotData) {
 		uint16_t currentSampleData = analogRead(analogDataPin);
-		for(uint8_t i = 0; i < numSamples; i++) {
+		for(uint8_t i = 0; i < lastNumSamples; i++) {
 			// shift the data to the left ...
 			analogData[i] = analogData[i + 1]; // TODO: possibly use a linked list instead
 		}
-		if(numSamples % 2 == 0) { // numSamples is even
-			middleSample = (analogData[numSamples/2+1] + analogData[numSamples/2-1])/2; // average the two middle points
+		if(lastNumSamples % 2 == 0) { // lastNumSamples is even
+			middleSample = (analogData[lastNumSamples/2+1] + analogData[lastNumSamples/2-1])/2; // average the two middle points
 		}
-		else { // numSamples is odd
-			middleSample = analogData[numSamples/2]; // example: numSamples = 7, numSamples/2 = 3 (truncates), middle sample is analogData[3].
+		else { // lastNumSamples is odd
+			middleSample = analogData[lastNumSamples/2]; // example: lastNumSamples = 7, lastNumSamples/2 = 3 (truncates), middle sample is analogData[3].
 		}
-		analogData[numSamples-1] = currentSampleData; // ... and add the current sample to the end
-		Serial.print(middleSample); // print current sample delayed by numSamples/2
+		analogData[lastNumSamples-1] = currentSampleData; // ... and add the current sample to the end
+		Serial.print(middleSample); // print current sample delayed by lastNumSamples/2
 		Serial.print(" ");
-		Serial.println(getGaussianAverage(analogData, numSamples, gaussianCurveCutoff), 3); // this should be compared to a reading delayed by numSamples/2
+		Serial.println(getGaussianAverage(analogData, lastNumSamples, lastGaussianCurveCutoff, recalculate), 3); // this should be compared to a reading delayed by lastNumSamples/2
 	}
 	else if(mode == plotCurve) {
 		for(uint8_t i = 0; i < numSamples; i++) {
@@ -60,14 +76,20 @@ void loop() {
 	}
 }
 
-float getGaussianAverage(uint16_t analogData[], uint8_t numSamples, float gaussianCurveCutoff) {
+float getGaussianAverage(uint16_t analogData[], uint8_t numSamples, float gaussianCurveCutoff, bool recalculate) {
 	float gaussianSum = 0;
-	float sum = 0;
+	static float sum = 0;
+	static float gaussianWeights[maxNumSamples]; // used to cache values if numSamples and gaussianCurveCutoff don't change
 
+	if(recalculate) {
+		sum = 0;
+	}
 	for(uint8_t i = 0; i < numSamples; i++) {
-		float gaussianWeight = calculateGaussian(i, numSamples, gaussianCurveCutoff);
-		gaussianSum += (analogData[i]*gaussianWeight);
-		sum += gaussianWeight;
+		if(recalculate) {
+			gaussianWeights[i] = calculateGaussian(i, numSamples, gaussianCurveCutoff);
+			sum += gaussianWeights[i];
+		}
+		gaussianSum += (analogData[i]*gaussianWeights[i]);
 	}
 	return gaussianSum / sum; // normalize
 }
@@ -90,12 +112,16 @@ void printGaussianWeights(uint8_t numSamples, float gaussianCurveCutoff) {
 		sum += gaussianWeights[i];
 	}
 
-	Serial.println();
-	Serial.println();
+	Serial.print("{");
 	for(uint8_t i = 0; i < numSamples; i++) {
-		Serial.println(gaussianWeights[i] / sum, 7);
+		Serial.print(gaussianWeights[i] / sum, 7);
+		if(i < numSamples - 1) {
+			Serial.print(", ");
+		}
 	}
+	Serial.print("}");
 
+	Serial.println();
 	Serial.println();
 	Serial.print("Number of samples: ");
 	Serial.print(numSamples);
